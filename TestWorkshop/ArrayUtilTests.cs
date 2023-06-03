3a2bfd57249a43289c92cf403711b66a76488da1
DwarfPorter <mrvlmor@gmail.com>
2023-06-03 20:48:40 +0300

    добавлены тесты

--- TestWorkshop/ArrayUtilTests.cs
+++ TestWorkshop/ArrayUtilTests.cs
@@ -6,12 +6,60 @@ namespace TestWorkshop
     public class ArrayUtilTests
     {
         [TestMethod]
-        public void TestSecondMax1()
+        public void TestSecondMax_BeforeMax()
         {
-            int[] arr = new int[] { 4, 5, 5, 3};
+            int[] arr = new int[] {1, 4, 5, 5, 3};
 
             int actual = ArrayUtil.SecondMax(arr);
-            Assert.AreEqual(42, actual);
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_AfterMax()
+        {
+            int[] arr = new int[] { 1, 2, 7, 6, 3 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_FirstPosition()
+        {
+            int[] arr = new int[] { 5, 2, 7, 4, 3 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(5, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_LastPosition()
+        {
+            int[] arr = new int[] { 5, 2, 10, 4, 3, 9 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(9, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_FirstTwoMax()
+        {
+            int[] arr = new int[] { 9, 9, 8, 5, 2 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(8, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_LastMax()
+        {
+            int[] arr = new int[] { 9, 9, 8, 5, 2, 10 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(9, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondMax_BetweenMax()
+        {
+            int[] arr = new int[] { 1, 9, 7, 9, 3 };
+            int actual = ArrayUtil.SecondMax(arr);
+            Assert.AreEqual(7, actual);
         }
     }
 }
--- Workshop/Program.cs
+++ Workshop/Program.cs
@@ -1,9 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Workshop;
-
-int[] arr = new int[] { 1, 2, 3 };
+﻿int[] arr = new int[] { 1, 2, 3 };
 int max2 = ArrayUtil.SecondMax(arr);
 Console.WriteLine(max2);
-
-
