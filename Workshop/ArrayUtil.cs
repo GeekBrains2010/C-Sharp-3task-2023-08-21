862a66c0e9dbaa1aab29b6795affbf5f459be9c6
DwarfPorter <mrvlmor@gmail.com>
2023-08-21 21:23:43 +0300

    просто максимум

--- Workshop/ArrayUtil.cs
+++ Workshop/ArrayUtil.cs
@@ -14,7 +14,15 @@ namespace Workshop
              * массив: [2, 4, 5, 3], результат: 4
              * массив: [2, 6, 5, 3], результат: 5
              */
-            return 0;
+            int max = array[0];
+            foreach(int item in array)
+            {
+                if (max < item)
+                {
+                    max = item;
+                }
+            }
+            return max;
         }
     }
 }
