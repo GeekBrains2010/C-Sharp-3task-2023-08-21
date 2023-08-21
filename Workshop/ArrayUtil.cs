6d3180d58836973064d40bd0d5f0fb9ab9a5a55b
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-08-21 21:37:48 +0300

    done

--- Workshop/ArrayUtil.cs
+++ Workshop/ArrayUtil.cs
@@ -14,7 +14,20 @@ namespace Workshop
              * массив: [2, 4, 5, 3], результат: 4
              * массив: [2, 6, 5, 3], результат: 5
              */
-            return 0;
+            int firstMax = 0;
+            int secondMax = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (firstMax < array[i]) firstMax = array[i];
+                if (secondMax > array[i] && secondMax < firstMax) secondMax = array[i];
+
+            }
+            return firstMax;
+        }
+        public static void Main(String[] arggs)
+        {
+            int[] array = { 2, 4, 5, 11, 7 };
+            Console.WriteLine(SecondMax(array));
         }
     }
 }
