477d557733ea5016861be64dee4eae02d4b2b4f1
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-08-22 08:04:11 +0300

    update task solution

--- Workshop/ArrayUtil.cs
+++ Workshop/ArrayUtil.cs
@@ -15,13 +15,15 @@ namespace Workshop
              * массив: [2, 6, 5, 3], результат: 5
              */
             int firstMax = array[0];
+            int tempMax = firstMax;
             int secondMax = int.MinValue;
             for (int i = 1; i < array.Length; i++)
             {
                 if (firstMax < array[i])
                 {
                     firstMax = array[i];
-                    secondMax = array[i - 1];
+                    secondMax = tempMax;
+                    tempMax = firstMax;
                 }
                 else if (array[i] != firstMax && array[i] > secondMax) secondMax = array[i];
             }
